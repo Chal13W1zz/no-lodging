e10e547760e339b9e43bf09ebe46ac4130eb07f0
Chal13W1zz <chaliewizz4@gmail.com>
2021-06-15 23:41:02 +0300

    recreated the Review model

--- db/migrate/20210615203849_create_reviews.rb
+++ db/migrate/20210615203849_create_reviews.rb
@@ -4,7 +4,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :description
       t.integer :score
-      t.belongs_to :airline, null: false, foreign_key: true
+      t.belongs_to :house, null: false, foreign_key: true
 
       t.timestamps
     end
