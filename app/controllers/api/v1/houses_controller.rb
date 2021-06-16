aa5cae13a6bbe3334e315a0445be3c26af3fd43a
Chal13W1zz <chaliewizz4@gmail.com>
2021-06-16 20:38:08 +0300

    added a house update method

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -7,11 +7,15 @@ module Api
 
                 render json: HouseSerializer.new(houses).serialized_json
             end
+
+
             def show
                 house = House.find_by(slug: params[:slug])
 
                 render json: HouseSerializer.new(house).serialized_json
             end
+
+
             def create
                 house = House.new(house_params)
 
@@ -22,10 +26,22 @@ module Api
                 end
             end
 
-            private
-            def house_params
-                params.require(:house).permit(:name, :image_url)
-            end
+
+            def update
+                house = House.find_by(slug: params[:slug])
+        
+                if house.update(house_params)
+                  render json: HouseSerializer.new(house).serialized_json
+                else
+                  render json: { error: airline.errors.messages }, status: 422
+                end
+              end
+
+
+              private
+              def house_params
+                  params.require(:house).permit(:name, :image_url)
+              end
         end
     end
 end
