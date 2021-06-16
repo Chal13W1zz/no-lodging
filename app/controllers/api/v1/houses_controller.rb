23ff07c00898ff3f91e991351e9354d3e300dd12
Chal13W1zz <chaliewizz4@gmail.com>
2021-06-16 19:49:48 +0300

    added an show method that renders a specific house using it's slug as the param

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -6,6 +6,11 @@ module Api
 
                 render json: HouseSerializer.new(houses).serialized_json
             end
+            def show
+                house = House.find_by(slug: params[:slug])
+
+                render json: HouseSerializer.new(house).serialized_json
+            end
         end
     end
 end
