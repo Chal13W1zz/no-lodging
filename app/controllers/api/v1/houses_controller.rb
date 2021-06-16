344a89dad214d6bfd01404aa8f873955535028bf
Chal13W1zz <chaliewizz4@gmail.com>
2021-06-16 19:36:00 +0300

    added an index method in the housecontroller that reads gets all the houses from the db and renders them as json using the HouseSerializer

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,7 +1,11 @@
 module Api
     module V1
         class HousesController < ApplicationController
-            
+            def index
+                houses = House.all
+
+                render json: HouseSerializer.new(houses).serialized_json
+            end
         end
     end
 end
